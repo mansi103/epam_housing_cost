441a85d56e990d2205ea98ce639e281b6cc36a13
mansi103 <36470888+mansi103@users.noreply.github.com>
2019-03-19 10:09:59 +0530

    Update HousingCostServices.java

--- src/main/java/com/mansi/housingcost/services/HousingCostServices.java
+++ src/main/java/com/mansi/housingcost/services/HousingCostServices.java
@@ -1,5 +1,10 @@
 package com.mansi.housingcost.services;
-
+/**
+* @param area
+* @param Material
+* @param Automated
+* @return cost of house
+*/
 public interface HousingCostServices {
 	public double costofhouse(double area, String Material, String Automated);
 }
